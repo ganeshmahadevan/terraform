723a49dfa3382c58c001c87e50e1199fdf7b6b59
Ganesh <ganesh.jegadish@yahoo.com>
2022-11-21 11:46:34 +0530

    commi

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "webserver" {
+  ami               = "ami-0e6329e222e662a52"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-south-1a"
+  key_name = "ganesh1"
+  subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+# Security Group
+  vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
+  tags = {
+        Name = "webserver"
+  }
+}
+
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {    
+    default = "ap-south-1"
+}
+
+variable "AMI" {
+    default =  "ami-074dc0a6f6c764218"
+  }
+
+variable "instance_type" {    
+    default = "t2.micro"
+}
